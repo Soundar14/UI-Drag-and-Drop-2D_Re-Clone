a7a4282e6bc41a24b9caea9aa1a009c132a21ac0
Soundar3902 <soudarraj@vmokshagroup.com>
2023-07-12 17:16:28 +0530

    Modified Input Strenth

--- Assets/Scripts/Drag and Drop/InputProviderForScroll.cs
+++ Assets/Scripts/Drag and Drop/InputProviderForScroll.cs
@@ -5,15 +5,16 @@ using UnityEngine.EventSystems;
 
 public class InputProviderForScroll : EventTrigger
 {
+    public float inputStrength = 0.5f;
     public override void OnDrag(PointerEventData eventData)
     {
         //Debug.Log(eventData.delta);
 
         Vector2 delta = eventData.delta;
 
-        float value = -delta.x / 10;
+        float value = -delta.x / 20;
 
-        DragNotifier.Instance.scroller.value+= value*Time.deltaTime;
+        DragNotifier.Instance.scroller.value+= value*Time.deltaTime*inputStrength;
 
     }
 }
